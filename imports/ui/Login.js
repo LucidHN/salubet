cefae3aa1f17381fb965608b71682b0f5fa13034
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2019-02-22 16:06:16 -0600

    login and signup

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,26 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 
-export default class Login extends React.Component {
+import { history } from '../routes/routes';
+
+export class Login extends React.Component {
     state = {
         email: '',
         errorMessage: false,
         loading: false,
         password: ''
     }
+    onSubmit = (event) => {
+        event.preventDefault();
+        let email = this.state.email.trim();
+        let password = this.state.password;
+        this.props.loginWithPassword({ email }, password, (err) => {
+            if (err) {
+                this.setState({error: 'Unable to login. Check email and password.'});
+            }else {
+                this.setState({error: ''});
+                history.push('/dashboard');
+            }
+        });
+    }
     render() {
         return (
             <div className="container absolute-center">
                 <div className="row justify-content-md-center">
                     <div className="col-sm-12 col-md-6 col-lg-6 align-self-center">
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <h2>Login</h2>
                             <div className="form-group">
                                 <label>Email address</label>
-                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <input 
+                                    type="email" 
+                                    className="form-control" 
+                                    id="email" 
+                                    aria-describedby="emailHelp" 
+                                    placeholder="Enter email"
+                                    onChange={(event) => this.setState({ email: event.target.value })}
+                                />
                             </div>
                             <div className="form-group">
                                 <label>Password</label>
-                                <input type="password" className="form-control" id="password" placeholder="Password" />
+                                <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    id="password" 
+                                    placeholder="Password" 
+                                    onChange={(event) => this.setState({ password: event.target.value })}
+                                    />
                             </div>
                             <button type="submit" className="btn btn-outline-primary btn-block">Submit</button>
                         </form>
@@ -29,4 +58,9 @@ export default class Login extends React.Component {
             </div>
         );
     }
-}
+};
+
+export default withTracker(() => ({
+    loginWithPassword: Meteor.loginWithPassword
+}))(Login);
+
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,37 +1,88 @@
 import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
 
-export default class Signup extends React.Component {
+import { history } from '../routes/routes';
+
+export class Signup extends React.Component {
     state = {
         email: '',
         password: '',
+        confirmPassword: '',
         name: '',
+        medicalCenterType: '',
         medicalCenter: '',
-        secretQuestion: '',
-        secretAnswer: '',
         errorMessage: false,
         loading: false
-
     }
-
+    onSubmit = async (event) => {
+        event.preventDefault();
+        if (this.state.password === this.state.confirmPassword) {
+            const user = {
+                email: this.state.email,
+                password: this.state.password,
+                profile: {
+                    name: this.state.name,
+                    medicalCenterType: this.state.medicalCenterType,
+                    medicalCenter: this.state.medicalCenter
+                }
+            };
+            await this.props.call('user.createUser', user, (error) => {
+                if (error) {
+                    this.setState({ 
+                        loading: false,  
+                        errorMessage: error.message 
+                    }); 
+                } else {
+                    this.setState({ loading: false });
+                    history.push('/login');
+                }
+            });
+        }
+    }
+    handleMedicalCenterDropdown = (event, medicalCenter) => {
+        event.preventDefault();
+        this.setState({ medicalCenter });
+    }
     render() {
         return (
             <div className="container absolute-center">
                 <div className="row justify-content-md-center">
                     <div className="col-sm-12 col-md-6 col-lg-6 align-self-center">
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <h2>Signup</h2>
                             <div className="form-group">
                                 <label>Name</label>
-                                <input type="text" className="form-control" id="name" aria-describedby="userName" placeholder="Enter full name" />
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    id="name" aria-describedby="userName" 
+                                    placeholder="Enter full name"
+                                    onChange={(event) => this.setState({ name: event.target.value })} 
+                                />
                             </div>
                             <div className="form-group">
                                 <label>Medical Center</label>
                                 <div className="form-check  mb-1  ">
-                                    <input className="form-check-input" type="radio" name="medicalCenterType" id="hospital" value="hospital" />
+                                    <input 
+                                        className="form-check-input" 
+                                        type="radio" 
+                                        name="medicalCenterType" 
+                                        id="hospital" 
+                                        value="hospital"
+                                        onChange={(event) => this.setState({ medicalCenterType: event.target.value })}  
+                                    />
                                     <label className="form-check-label" htmlFor="hospital">Hospital</label>
                                 </div>
                                 <div className="form-check mb-1">
-                                    <input className="form-check-input" type="radio" name="medicalCenterType" id="clinic" value="clinic" />
+                                    <input 
+                                        className="form-check-input" 
+                                        type="radio" 
+                                        name="medicalCenterType" 
+                                        id="clinic" 
+                                        value="clinic" 
+                                        onChange={(event) => this.setState({ medicalCenterType: event.target.value })}
+                                    />
                                     <label className="form-check-label" htmlFor="clinic">Clinic</label>
                                 </div>
                                 <div className="dropdown mt-2 mb-2">
@@ -40,36 +91,47 @@ export default class Signup extends React.Component {
                                         Available Medical Centers
                                     </button>
                                     <div className="dropdown-menu" aria-labelledby="medicalCenter">
-                                        <a className="dropdown-item">Action</a>
-                                        <a className="dropdown-item">Action2</a>
-                                        <a className="dropdown-item">Action3</a>
+                                        <a 
+                                            className="dropdown-item"
+                                            onClick={(event) => (this.handleMedicalCenterDropdown(event, 'HMS'))}
+                                        >HMS</a>
+                                        <a 
+                                        className="dropdown-item"
+                                        onClick={(event) => (this.handleMedicalCenterDropdown(event, 'San Felipe'))}
+                                        >San Felipe</a>
                                     </div>
                                 </div>
                             </div>
                             <div className="form-group">
-                                <label>Secret Question</label>
-                                <input type="text" className="form-control" id="secretQuestion" aria-describedby="secretQuestion" placeholder="Enter secret question" />
-                            </div>
-                            <div className="form-group">
-                                <label>Secret Answer</label>
-                                <input type="password" className="form-control" id="secretAnswer" aria-describedby="secretAnswer" placeholder="Enter secret answer" />
-                            </div>
-                            <div className="form-group">
-                                <label>Confirm Secret Answer</label>
-                                <input type="password" className="form-control" id="confirmSecretAnswer" aria-describedby="secretAnswerConfirmation" placeholder="Confirm secret answer" />
-                            </div>
-
-                            <div className="form-group">
                                 <label>Email address</label>
-                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <input 
+                                    type="email" 
+                                    className="form-control" 
+                                    id="email" 
+                                    aria-describedby="emailHelp" 
+                                    placeholder="Enter email" 
+                                    onChange={(event) => this.setState({ email: event.target.value })}
+                                />
                             </div>
                             <div className="form-group">
                                 <label>Password</label>
-                                <input type="password" className="form-control" id="password" placeholder="Password" />
+                                <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    id="password" 
+                                    placeholder="Password"
+                                    onChange={(event) => this.setState({ password: event.target.value })} 
+                                />
                             </div>
                             <div className="form-group">
                                 <label>Confirm Password</label>
-                                <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm password" />
+                                <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    id="confirmPassword" 
+                                    placeholder="Confirm password" 
+                                    onChange={(event) => this.setState({ confirmPassword: event.target.value })}
+                                />
                             </div>
                             <button type="submit" className="btn btn-outline-primary btn-block mb-5">Submit</button>
                         </form>
@@ -78,4 +140,8 @@ export default class Signup extends React.Component {
             </div>
         );
     }
-}
+};
+
+export default withTracker(() => ({
+    call: Meteor.call
+}))(Signup);
