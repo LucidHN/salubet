220c34a85b3cf8adf1afb3878b04ed5d3204bab6
RGaldamez <ricardo.galdamez@gmail.com>
2019-02-22 13:56:58 -0600

    Adding SignUp Skeleton in Bootstrap 4

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -11,16 +11,16 @@ export default class Login extends React.Component {
         return (
             <div className="container absolute-center">
                 <div className="row justify-content-md-center">
-                    <div className="col-sm-12 col-md-5 col-lg-6 align-self-center">
+                    <div className="col-sm-12 col-md-6 col-lg-6 align-self-center">
                         <form>
                             <h2>Login</h2>
                             <div className="form-group">
                                 <label>Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
                             </div>
                             <div className="form-group">
                                 <label>Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                <input type="password" className="form-control" id="password" placeholder="Password" />
                             </div>
                             <button type="submit" className="btn btn-outline-primary btn-block">Submit</button>
                         </form>
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -10,19 +10,70 @@ export default class Signup extends React.Component {
         secretAnswer: '',
         errorMessage: false,
         loading: false
-        
+
     }
 
     render() {
         return (
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown button
-                </button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <a className="dropdown-item" href="#">Something else here</a>
+            <div className="container absolute-center">
+                <div className="row justify-content-md-center">
+                    <div className="col-sm-12 col-md-6 col-lg-6 align-self-center">
+                        <form>
+                            <h2>Signup</h2>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input type="text" className="form-control" id="name" aria-describedby="userName" placeholder="Enter full name" />
+                            </div>
+                            <div className="form-group">
+                                <label>Medical Center</label>
+                                <div className="form-check  mb-1  ">
+                                    <input className="form-check-input" type="radio" name="medicalCenterType" id="hospital" value="hospital" />
+                                    <label className="form-check-label" htmlFor="hospital">Hospital</label>
+                                </div>
+                                <div className="form-check mb-1">
+                                    <input className="form-check-input" type="radio" name="medicalCenterType" id="clinic" value="clinic" />
+                                    <label className="form-check-label" htmlFor="clinic">Clinic</label>
+                                </div>
+                                <div className="dropdown mt-2 mb-2">
+                                    
+                                    <button className="btn btn-primary dropdown-toggle btn-block" type="button" id="medicalCenter" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Available Medical Centers
+                                    </button>
+                                    <div className="dropdown-menu" aria-labelledby="medicalCenter">
+                                        <a className="dropdown-item">Action</a>
+                                        <a className="dropdown-item">Action2</a>
+                                        <a className="dropdown-item">Action3</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label>Secret Question</label>
+                                <input type="text" className="form-control" id="secretQuestion" aria-describedby="secretQuestion" placeholder="Enter secret question" />
+                            </div>
+                            <div className="form-group">
+                                <label>Secret Answer</label>
+                                <input type="password" className="form-control" id="secretAnswer" aria-describedby="secretAnswer" placeholder="Enter secret answer" />
+                            </div>
+                            <div className="form-group">
+                                <label>Confirm Secret Answer</label>
+                                <input type="password" className="form-control" id="confirmSecretAnswer" aria-describedby="secretAnswerConfirmation" placeholder="Confirm secret answer" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Email address</label>
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                            </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type="password" className="form-control" id="password" placeholder="Password" />
+                            </div>
+                            <div className="form-group">
+                                <label>Confirm Password</label>
+                                <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm password" />
+                            </div>
+                            <button type="submit" className="btn btn-outline-primary btn-block mb-5">Submit</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         );
