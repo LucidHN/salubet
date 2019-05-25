64d71fb297a16dbf9cf6c65a20635c8daa9fd234
Ricardo <ricardo.j.galdamez@gmail.com>
2019-05-25 15:46:42 -0600

    Fixing subheader color for mainSidebar

--- imports/ui/MainSidebar.js
+++ imports/ui/MainSidebar.js
@@ -17,7 +17,7 @@ export class MainSidebar extends React.Component {
                         <h3>Dr. { this.props.user ? this.props.user.profile.name : null }</h3>
                     </div>
                     <ul className="list-unstyled components bar">
-                        <p className="bar">San Felipe</p>
+                        <p className="bar hospital-subheader">San Felipe</p>
                         <li className="bar">
                             <a className="bar" href="#">Información General</a>
                         </li>
